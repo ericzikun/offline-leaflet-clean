2669375b7342dfb59824b3320968dd873c9699b4
popofzk <847473488@qq.com>
2021-06-29 10:54:32 +0800

    offline-leaflet-clean

---
+++ json/circleCenter.js
@@ -0,0 +1,19 @@
+var circleCenter  = [{
+  "type": "Feature",
+  "properties": {"id": "1","color":"#BB443E","radius":[40000]},
+  "geometry": {
+       
+      "coordinates": [[
+        [ 25.2773125506499,124.687775915409]
+      ]]
+  }
+}, {
+  "type": "Feature",
+  "properties": {"id": "2","color":"#000000","radius":[80000]},
+  "geometry": {
+       
+      "coordinates": [[
+        [ 17.9228934895459,111.584433349023]
+      ]]
+  }
+}]
